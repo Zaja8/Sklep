b625215023b5775255766980fe4b1d81e4e99eb6
jzajac <jzajac@DMT-KRK-L023>
2018-11-12 17:20:12 +0100

    Zadanie tydzień 2

--- Documents/NetBeansProjects/sklep/src/sklep/Sklep.java
+++ Documents/NetBeansProjects/sklep/src/sklep/Sklep.java
@@ -17,7 +17,57 @@ public class Sklep
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        user Adam = new user();
+        System.out.println(Adam.getLogin());
     }
     
+}
+
+class user
+{
+    private int id;
+    private String login;
+    private String password;
+    
+    int getId(){return this.id;}
+    String getLogin(){return this.login;}
+    String getPassword(){return this.password;}
+}
+
+class Product
+{
+    private int id;
+    private String productName;
+    private float price;
+    private float weight;
+    private String color;
+    private int productCount;
+    
+    int getId(){return this.id;}
+    String getProductName(){return this.productName;}
+    float getPrice(){return this.price;}
+    float getWeight(){return this.weight;}
+    String getColor(){return this.color;}
+    int getProductCount(){return this.productCount;}
+    
+    void setPrice(float price){this.price = price;}
+    void setProductCount(int productCount){this.productCount = productCount;}
+}
+
+class Boots extends Product
+{
+    private int size;
+    private boolean isNaturalLeather;
+    
+    int getSize(){return this.size;}
+    boolean getIsNaturalLeather(){return this.isNaturalLeather;}
+}
+
+class Cloth extends Product
+{
+    private String size;
+    private String material;
+    
+    String getSize(){return this.size;}
+    String getMaterial(){return this.material;}
 }
